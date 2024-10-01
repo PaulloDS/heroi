5132334fd66f3795afe71e59e2eadfdb735cdbcc
PaulloDS <122841064+PaulloDS@users.noreply.github.com>
2024-10-01 16:10:31 -0300

    Update heroi.js

--- heroi.js
+++ heroi.js
@@ -7,8 +7,10 @@ function classificarHeroi(nome, xp) {
     } else if (xp >= 1000 && xp <= 2000) {
         rank = 'Bronze';
     } else if (xp > 2000 && xp <= 5000) {
-        rank = 'Prata Ouro';
-    } else if (xp > 5000 && xp <= 8000) {
+        rank = 'Prata ';
+    } else if (xp > 6000 && xp <= 6000){
+        rank = 'Ouro'
+    } else if (xp > 7000 && xp <= 8000) {
         rank = 'Platina Diamante';
     } else if (xp > 8000 && xp <= 9000) {
         rank = 'Ascendente';
