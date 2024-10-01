d4c8f663a3f33b4f71323f316e518d61b9307966
PaulloDS <122841064+PaulloDS@users.noreply.github.com>
2024-10-01 16:07:35 -0300

    Initial commit

---
+++ heroi.js
@@ -0,0 +1,24 @@
+// Função para classificar o herói com base na XP
+function classificarHeroi(nome, xp) {
+    let rank;
+
+    if (xp < 1000) {
+        rank = 'Ferro';
+    } else if (xp >= 1000 && xp <= 2000) {
+        rank = 'Bronze';
+    } else if (xp > 2000 && xp <= 5000) {
+        rank = 'Prata Ouro';
+    } else if (xp > 5000 && xp <= 8000) {
+        rank = 'Platina Diamante';
+    } else if (xp > 8000 && xp <= 9000) {
+        rank = 'Ascendente';
+    } else if (xp > 9000 && xp <= 10000) {
+        rank = 'Imortal';
+    } else if (xp >= 10001) {
+        rank = 'Radiante';
+    }
+
+    console.log(`Herói: ${nome}, XP: ${xp}, Rank: ${rank}`);
+}
+
+classificarHeroi("Paullo", 5500);
